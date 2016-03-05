9c793ad496d35afb2787045f6f2fb13eb3148b6f
ytet5uy4 <ytet5uy4@outlook.com>
2016-03-06 06:32:14 +0900

    コールバックcontrollerの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:twitter]
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+    unless user
+      user = User.create(
+        uid: auth.uid,
+        provider: auth.provider,
+        email: User.get_email(auth),
+        password: Devise.friendly_token[4, 30])
+    end
+    user
+  end
+
+  private
+    def self.get_email(auth)
+      email = auth.info.email
+      email = "#{auth.provider}-#{auth.uid}@example.com" if email.blank?
+      email
+    end
 end
